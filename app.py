1958bade7138d1a21c151057961e1d0fc5533a73
wellbar2 <wellbar2@yahoo.com.br>
2021-08-19 23:45:00 +0000

    commit

--- app.py
+++ app.py
@@ -10,7 +10,21 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 def conectar_bd():
-    return sqlite3.connect(app.config['DATABSE'])
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def depois_requisicao(exc):
+    g.bd.close()
+
+
+@app.route('/')
+def exibir_entradas():
+    return "<h1>Aqui estarão as postagens</h1>"
 
 @app.route('/hello')
 def pagina_inicial():
